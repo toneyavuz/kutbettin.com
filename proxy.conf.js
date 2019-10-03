a6c5e3d85b4d6e316e2598304a6be0f327122e2b
kyavuz <yavuz@kutbettin.com>
2019-10-03 12:12:11 +0300

    initial commit

---
+++ proxy.conf.js
@@ -0,0 +1,29 @@
+const selector = JSON.parse(process.env['npm_config_argv'])['original'][0];
+
+const apiTarget = {
+  'start': 'http://172.20.10.2:81/',
+  'build': '/api',
+};
+
+const rewrite = {
+  // 'start': {pathRewrite: {'^/api': ''}}
+};
+
+const modeSelector = (pathSource) => {
+  return pathSource[selector];
+};
+
+const PROXY_CONFIG = [
+  {
+    context: [
+      '/api'
+    ],
+    target: `${ modeSelector(apiTarget) }`,
+    secure: false,
+    changeOrigin: true,
+    logLevel: 'debug',
+  }
+];
+
+PROXY_CONFIG[0] = {...rewrite[selector], ...PROXY_CONFIG[0]};
+module.exports = PROXY_CONFIG;
